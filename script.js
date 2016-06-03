d77c597b3a19e222765513e9cdcc8e45f56b6159
AtillaSzikora <szikora.atilla@gmail.com>
2016-06-03 19:52:48 +0200

    Url validation works for now. :)

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-var addButton = document.getElementById("addButton");
 var url = document.getElementById("url");
 var newsCount = document.getElementById("newsCount");
+var addButton = document.getElementById("addButton");
 
 function disableButton() {
-    if (newsCount.value < 1
-        || url.checkValidity() == false
-        || url.value.substring(this.value.length - 4) != ".xml")
-        { addButton.disabled = true; }
-    else {addButton.disabled = false; } }
+    addButton.disabled = !!(newsCount.value < 1
+                         || url.checkValidity() == false
+                         || url.value.substring(url.value.length - 4) != ".xml");
+}
 
 function urlBorderColor() {
-    if (this.checkValidity() == false ||
-        this.value.substring(this.value.length - 4) != ".xml") {
-        url.classList.remove("greenBorder");
-        url.classList.add("redBorder"); }
+    if (url.checkValidity() == false ||
+        url.value.substring(url.value.length - 4) != ".xml") {
+        url.parentNode.classList.remove("has-success");
+        url.parentNode.classList.add("has-error");
+    }
     else {
-        url.classList.remove("redBorder");
-        url.classList.add("greenBorder"); } }
+        url.parentNode.classList.remove("has-error");
+        url.parentNode.classList.add("has-success");
+    }
+}
 
 function newsCountBorderColor() {
-    if (this.value < 1) {
-        newsCount.classList.remove("greenBorder");
-        newsCount.classList.add("redBorder"); }
+    if (newsCount.value < 1) {
+        newsCount.parentNode.classList.remove("has-success");
+        newsCount.parentNode.classList.add("has-error");
+    }
     else {
-        newsCount.classList.remove("redBorder");
-        newsCount.classList.add("greenBorder"); } }
+        newsCount.parentNode.classList.remove("has-error");
+        newsCount.parentNode.classList.add("has-success");
+    }
+}
 
 url.addEventListener("keyup", disableButton);
 url.addEventListener("keyup", urlBorderColor);
 url.addEventListener("click", disableButton);
-url.addEventListener("click", newsCountBorderColor);
+url.addEventListener("click", urlBorderColor);
 newsCount.addEventListener("keyup", disableButton);
 newsCount.addEventListener("keyup", newsCountBorderColor);
 newsCount.addEventListener("click", disableButton);
