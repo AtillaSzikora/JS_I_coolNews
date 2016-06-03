027610506587f74463c9d939ef1c587d61f2d5f6
atillaahun <szikora.atilla@gmail.com>
2016-06-03 16:54:49 +0200

    newsCount validation sucks

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 var addButton = document.getElementById("addButton");
 var url = document.getElementById("url");
+var newsCount = document.getElementById("newsCount");
 
-function disableButton(bool) {
-    addButton.disabled = bool; }
+function disableButton() {
+    if (newsCount.value < 1
+        || url.checkValidity() == false
+        || url.value.substring(this.value.length - 4) != ".xml")
+        { addButton.disabled = true; }
+    else {addButton.disabled = false; } }
 
-function borderColor() {
-    if (this.checkValidity() == false || this.value.substring(this.value.length - 4) != ".xml") {
+function urlBorderColor() {
+    if (this.checkValidity() == false ||
+        this.value.substring(this.value.length - 4) != ".xml") {
         url.classList.remove("greenBorder");
-        url.classList.add("redBorder");
-        disableButton(true); }
+        url.classList.add("redBorder"); }
     else {
         url.classList.remove("redBorder");
-        url.classList.add("greenBorder");
-        disableButton(false); } }
+        url.classList.add("greenBorder"); } }
+
+function newsCountBorderColor() {
+    if (this.value < 1) {
+        newsCount.classList.remove("greenBorder");
+        newsCount.classList.add("redBorder"); }
+    else {
+        newsCount.classList.remove("redBorder");
+        newsCount.classList.add("greenBorder"); } }
 
 url.addEventListener("keyup", disableButton);
-url.addEventListener("keyup", borderColor);
+url.addEventListener("keyup", urlBorderColor);
+url.addEventListener("click", disableButton);
+url.addEventListener("click", newsCountBorderColor);
+newsCount.addEventListener("keyup", disableButton);
+newsCount.addEventListener("keyup", newsCountBorderColor);
+newsCount.addEventListener("click", disableButton);
+newsCount.addEventListener("click", newsCountBorderColor);
