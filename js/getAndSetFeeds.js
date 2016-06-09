5737787b8d2d99d7ae9de6af179d91fb405d71da
AtillaSzikora <szikora.atilla@gmail.com>
2016-06-09 22:40:49 +0200

    Never use innerHTML because it fucks up your life for good. Use insertAdjacentHTML instead.

--- js/getAndSetFeeds.js
+++ js/getAndSetFeeds.js
@@ -3,7 +3,28 @@ var feedName = document.getElementById("feedName");
 var newsCount = document.getElementById("newsCount");
 var addButton = document.getElementById("addButton");
 
-function localStoreFeed() {
+var feedStr1;
+var feedObj1;
+var jumbo = document.getElementsByClassName("jumbotron");
+var formRow = '<div class="form">\
+                    <div class="form-group"><input type="url" class="form-control"></div>\
+                    <div class="form-group"><input type="text" class="form-control"></div>\
+                    <div class="form-group"><input type="number" class="form-control"></div>\
+                </div>';
+
+for (var i = 0; i < localStorage.length; i++) {
+    jumbo[0].insertAdjacentHTML('beforeend', formRow);
+}
+var form = document.getElementsByClassName("form");
+for (var j = 0; j < localStorage.length; j++) {
+    feedStr1 = localStorage.getItem((j + 1).toString());
+    feedObj1 = JSON.parse(feedStr1);
+    form[j].getElementsByTagName("input")[0].value = feedObj1.url;
+    form[j].getElementsByTagName("input")[1].value = feedObj1.feedName;
+    form[j].getElementsByTagName("input")[2].value = feedObj1.newsCount;
+}
+
+function listFeeds() {
     var localStorageLength = (localStorage.length + 1).toString();
     var feedObj = {
         url: url.value,
@@ -12,28 +33,17 @@ function localStoreFeed() {
     };
     var feedStr = JSON.stringify(feedObj);
     localStorage.setItem(localStorageLength, feedStr);
+    var feedStr1;
+    var feedObj1;
+    jumbo[0].insertAdjacentHTML('beforeend', formRow);
+
+    var form = document.getElementsByClassName("form");
+    var formLength = form.length - 1;
+        feedStr1 = localStorage.getItem((formLength + 1).toString());
+        feedObj1 = JSON.parse(feedStr1);
+        form[formLength].getElementsByTagName("input")[0].value = feedObj1.url;
+        form[formLength].getElementsByTagName("input")[1].value = feedObj1.feedName;
+        form[formLength].getElementsByTagName("input")[2].value = feedObj1.newsCount;
 }
 
-function listFeeds() {
-    var feedStr;
-    var feedObj;
-    var jumbo = document.getElementsByClassName("jumbotron");
-    var formFields = '<div class="form">\
-                            <div class="form-group"><input type="url" class="form-control"></div>\
-                            <div class="form-group"><input type="text" class="form-control"></div>\
-                            <div class="form-group"><input type="number" class="form-control"></div>\
-                          </div>';
-    for (var i = 0; i < localStorage.length; i++) {
-        jumbo[0].innerHTML += formFields;
-    }
-    var form = document.getElementsByClassName("form");
-    for (var j = 1; j < form.length; j++) {
-        feedStr = localStorage.getItem((j).toString());
-        feedObj = JSON.parse(feedStr);
-        form[j].getElementsByTagName("input")[0].value = feedObj.url;
-        form[j].getElementsByTagName("input")[1].value = feedObj.feedName;
-        form[j].getElementsByTagName("input")[2].value = feedObj.newsCount;
-    }
-}
-addButton.addEventListener("click", localStoreFeed);
 addButton.addEventListener("click", listFeeds);
--- js/validateInputs.js
+++ js/validateInputs.js
@@ -5,44 +5,33 @@ var addButton = document.getElementById("addButton");
 
 function disableButton() {
     addButton.disabled = !!(newsCount.value < 1
-    || feedName.value.length == 0
-    || url.checkValidity() == false
-    || url.value.substring(url.value.length - 4) != ".xml");
-}
+                         || feedName.value.length == 0
+                         || url.checkValidity() == false
+                         || url.value.substring(url.value.length - 4) != ".xml"); }
 
 function urlBorderColor() {
-    if (url.checkValidity() == false ||
-        url.value.substring(url.value.length - 4) != ".xml") {
+    if (url.checkValidity() == false || url.value.substring(url.value.length - 4) != ".xml") {
         url.parentNode.classList.remove("has-success");
-        url.parentNode.classList.add("has-error");
-    }
+        url.parentNode.classList.add("has-error"); }
     else {
         url.parentNode.classList.remove("has-error");
-        url.parentNode.classList.add("has-success");
-    }
-}
+        url.parentNode.classList.add("has-success"); } }
 
 function feedNameBorderColor() {
     if (feedName.value.length == 0) {
         feedName.parentNode.classList.remove("has-success");
-        feedName.parentNode.classList.add("has-error");
-    }
+        feedName.parentNode.classList.add("has-error"); }
     else {
         feedName.parentNode.classList.remove("has-error");
-        feedName.parentNode.classList.add("has-success");
-    }
-}
+        feedName.parentNode.classList.add("has-success"); } }
 
 function newsCountBorderColor() {
     if (newsCount.value < 1) {
         newsCount.parentNode.classList.remove("has-success");
-        newsCount.parentNode.classList.add("has-error");
-    }
+        newsCount.parentNode.classList.add("has-error"); }
     else {
         newsCount.parentNode.classList.remove("has-error");
-        newsCount.parentNode.classList.add("has-success");
-    }
-}
+        newsCount.parentNode.classList.add("has-success"); } }
 
 newsCount.value = 5;
 url.addEventListener("blur", disableButton);
