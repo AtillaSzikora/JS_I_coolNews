e6622e355011edbc9663ee9763af2f19d5da5d9f
atillaahun <szikora.atilla@gmail.com>
2016-06-10 11:53:19 +0200

    getAndSetFeeds.js functions reorganized and cleared.

--- js/getAndSetFeeds.js
+++ js/getAndSetFeeds.js
@@ -2,48 +2,48 @@ var url = document.getElementById("url");
 var feedName = document.getElementById("feedName");
 var newsCount = document.getElementById("newsCount");
 var addButton = document.getElementById("addButton");
-
-var feedStr1;
-var feedObj1;
 var jumbo = document.getElementsByClassName("jumbotron");
 var formRow = '<div class="form">\
-                    <div class="form-group"><input type="url" class="form-control"></div>\
+                    <div class="form-group url"><input type="url" class="form-control"></div>\
                     <div class="form-group"><input type="text" class="form-control"></div>\
                     <div class="form-group"><input type="number" class="form-control"></div>\
                 </div>';
 
-for (var i = 0; i < localStorage.length; i++) {
-    jumbo[0].insertAdjacentHTML('beforeend', formRow);
-}
-var form = document.getElementsByClassName("form");
-for (var j = 0; j < localStorage.length; j++) {
-    feedStr1 = localStorage.getItem((j + 1).toString());
-    feedObj1 = JSON.parse(feedStr1);
-    form[j].getElementsByTagName("input")[0].value = feedObj1.url;
-    form[j].getElementsByTagName("input")[1].value = feedObj1.feedName;
-    form[j].getElementsByTagName("input")[2].value = feedObj1.newsCount;
-}
+function listFeedsFromStorage() {
+    var feedStr;
+    var feedObj;
+    var localStorageLength = localStorage.length;
+    for (var i = 0; i < localStorageLength; i++) {
+        jumbo[0].insertAdjacentHTML('beforeend', formRow); }
+    var formsArray = document.getElementsByClassName("form");
+    var formsArrayLength = formsArray.length;
+    for (var j = 1; j < formsArrayLength; j++) {
+        feedStr = localStorage.getItem((j).toString());
+        feedObj = JSON.parse(feedStr);
+        formsArray[j].getElementsByTagName("input")[0].value = feedObj.url;
+        formsArray[j].getElementsByTagName("input")[1].value = feedObj.feedName;
+        formsArray[j].getElementsByTagName("input")[2].value = feedObj.newsCount; } }
 
-function listFeeds() {
+function storeNewFeed() {
     var localStorageLength = (localStorage.length + 1).toString();
     var feedObj = {
         url: url.value,
         feedName: feedName.value,
-        newsCount: newsCount.value
-    };
+        newsCount: newsCount.value };
     var feedStr = JSON.stringify(feedObj);
     localStorage.setItem(localStorageLength, feedStr);
-    var feedStr1;
-    var feedObj1;
+    listNewFeed(); }
+
+function listNewFeed() {
     jumbo[0].insertAdjacentHTML('beforeend', formRow);
+    var formsArray = document.getElementsByClassName("form");
+    var formsArrayLength = formsArray.length;
+    var localStorageLength = (localStorage.length).toString();
+    var feedStr = localStorage.getItem(localStorageLength);
+    var feedObj = JSON.parse(feedStr);
+        formsArray[formsArrayLength - 1].getElementsByTagName("input")[0].value = feedObj.url;
+        formsArray[formsArrayLength - 1].getElementsByTagName("input")[1].value = feedObj.feedName;
+        formsArray[formsArrayLength - 1].getElementsByTagName("input")[2].value = feedObj.newsCount; }
 
-    var form = document.getElementsByClassName("form");
-    var formLength = form.length - 1;
-        feedStr1 = localStorage.getItem((formLength + 1).toString());
-        feedObj1 = JSON.parse(feedStr1);
-        form[formLength].getElementsByTagName("input")[0].value = feedObj1.url;
-        form[formLength].getElementsByTagName("input")[1].value = feedObj1.feedName;
-        form[formLength].getElementsByTagName("input")[2].value = feedObj1.newsCount;
-}
-
-addButton.addEventListener("click", listFeeds);
+addButton.addEventListener("click", storeNewFeed);
+listFeedsFromStorage();
